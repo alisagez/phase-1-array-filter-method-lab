b4264ef813bfadb5bde14a41c0322eecba753614
Alisa Gezalova <alisa.gezalova@gmail.com>
2022-02-13 21:30:15 -0700

    commit tree 1e255808433e200685d8da1ae056ae6e605fa96e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+//toLowerCase is added to pass the test that asks for a return of matching drivers if case does not match, but letters do
+
+//this is the full findMatch function. it is rewritten below as an arrow function
+// function findMatching(drivers, name) {
+//     let resultMatch = drivers.filter(function(driver){
+//     return driver.toLowerCase() === name.toLowerCase()})
+//     return resultMatch
+// }
+
+function findMatching(drivers, name) {
+    let resultMatch = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    return resultMatch
+}
+
+
+function fuzzyMatch(drivers, firstCharacters) {
+    let resultMatch = drivers.filter(driver => driver.startsWith(firstCharacters))
+    return resultMatch
+}
+
+function matchName(drivers, enteredCharacters) {
+    let resultMatch = drivers.filter(driver => driver.name === enteredCharacters)
+    return resultMatch
+}
